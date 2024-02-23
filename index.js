b4b333b4bffa69c4a33c6290b5e6a7d7e65dc61f
au143 <you@example.com>
2024-02-23 15:45:32 +0500

    awsdeployment

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ const express = require("express");
 // const bodyParser = require('body-parser');
 const cors = require("cors");
 const { connectToDatabase } = require("./database/dbConnection");
-// const { usersRouter } = require("./routes/usersRouter");
-// const { businessRouter } = require("./routes/businessRouter ");
-// const { clientRouter } = require("./routes/clientRouter");
-// const { invoiceRouter } = require("./routes/invoiceRouter");
-// const { uploadRouter } = require("./routes/uploadRouter");
-// const { emailRouter } = require("./routes/emailRouter");
+
 const { router } = require("./routes/index");
 // const pdfRoutes = require("./routes/pdfRoutes");
 require("dotenv").config();
@@ -32,15 +27,6 @@ app.use(express.json());
 
 app.use('/api', router);
 
-// app.use('/*', (req,res)=>res.send('404 errdcaor'))
-// app.use("/login", usersRouter);
-// app.use("/business", businessRouter);
-// app.use("/client", clientRouter);
-// app.use("/invoices", invoiceRouter);
-// app.use("/upload", uploadRouter);
-// app.use("/email", emailRouter);
-// app.use("/pdf", pdfRoutes);
-// app.use('/*', (req, res) => res.send('X-Invoice Backend Routing Not Found'))
 const appPromise= async()=>{
   const PORT = process.env.PORT || 9001;
   await connectToDatabase();
--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,17 @@ const clientRouter = require('./clientRouter')
 const usersRouter = require('./usersRouter')
 const businessRouter = require('./businessRouter ')
 const invoiceRouter = require('./invoiceRouter')
-// const pdfRouter = require('./pdfRouter')
+const pdfRouter = require('./pdfRouter')
 const uploadRouter = require('./uploadRouter')
 
 const router= express.Router()
 
-router.use('/users', usersRouter.usersRouter)
+router.use('/user', usersRouter.usersRouter)
 router.use('/business', businessRouter.businessRouter)
 router.use('/client', clientRouter.clientRouter)
 router.use('/invoice', invoiceRouter.invoiceRouter)
 router.use('/upload', uploadRouter.uploadRouter)
 router.use('/email', emailRouter.emailRouter)
-// router.use('/pdf', pdfRouter.pdfRouter)
+router.use('/pdf', pdfRouter.pdfRouter)
 
 module.exports= {router: router}
